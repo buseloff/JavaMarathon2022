1a1b7aa58076349434c1afabd5e3c94418ec2685
Konstantin <safoshkinkonstantin@gmail.com>
2020-09-28 01:23:49 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,50 @@
+/*
+1. Создать класс Автомобиль (англ. Car), с полями “Год выпуска”, “Цвет”, “Модель”.
+Создать геттеры и сеттеры для каждого поля. Создать экземпляр класса Автомобиль,
+задать сеттером каждое поле, вывести в консоль значение каждого поля геттером.
+Созданный вами класс должен отвечать принципам инкапсуляции.
+
+ */
 package day5;
+
+class Car {
+    private int yearOfManufacture;
+    private String color;
+    private String model;
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car audi = new Car();
+        audi.setYearOfManufacture(2017);
+        System.out.println(audi.getYearOfManufacture());
+        audi.setColor("Blue");
+        System.out.println(audi.getColor());
+        audi.setModel("A4");
+        System.out.println(audi.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,7 +1,43 @@
+/*
+2. Создать класс Мотоцикл (англ. Motorbike),
+с  полями “Год выпуска”, “Цвет”, “Модель”.
+Создать экземпляр класса Мотоцикл, с помощью конструктора (сеттеры не использовать).
+Придерживаться принципов инкапсуляции и использовать ключевое слово this.
+Геттером получить год выпуска, цвет, модель, вывести значения в консоль.
+
+ */
 package day5;
+
+class Motorbike {
+    private final int yearOfManufacture;
+    private final String color;
+    private final String model;
+
+    public Motorbike(int yearOfManufacture, String color, String model) {
+        this.yearOfManufacture = yearOfManufacture;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+}
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike honda = new Motorbike(2020, "Red", "CBR650R");
+        System.out.println(honda.getYearOfManufacture());
+        System.out.println(honda.getColor());
+        System.out.println(honda.getModel());
     }
 }
