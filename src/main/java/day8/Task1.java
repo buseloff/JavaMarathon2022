531e7e2a2c0de57c2849c96062c2a294e4bf2ef3
Konstantin <safoshkinkonstantin@gmail.com>
2020-10-04 01:31:01 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,10 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        StringBuilder numbers = new StringBuilder();
+        for (int i = 0; i <= 20000;i++) {
+            numbers.append(i).append(" ");
+        }
+        System.out.println(numbers);
     }
 }
